b8bc8ee9c2afc25a05c7b6a96b45ca1bae93dbde
l0v3-alek <bredemeier0402@gmail.com>
2026-05-14 11:26:10 +0200

    Change primary and secondary colors in bot config
    Swap primary and secondary bot colors.

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#006400", 
-      secondary: "#ecebbd", 
+      primary: "#ecebbd", 
+      secondary: "#006400", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
